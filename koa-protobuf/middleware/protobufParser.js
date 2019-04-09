008da6b4ee6b3e7f749f02c997fe16a45d5ee49e
medusa,leee <medusa.leee@gmail.com>
2019-04-10 00:03:57 +0800

    [feat] add koa protobuf parse middleware

--- koa-protobuf/middleware/protobufParser.js
+++ koa-protobuf/middleware/protobufParser.js
@@ -1,4 +1,27 @@
+const getRawBody = require('raw-body')
 
 const protobufParser = async (ctx, next) => {
-
+    const p = /^application\/protobuf;/
+    const contentType = ctx.get('content-type')
+    if (!contentType) {
+        return await next()
+    }
+    if (p.test(contentType.toLowerCase())) {
+        await getRawBody(ctx.req)
+        return await next()
+    }
+    await next()
 }
+
+// await new Promise((resolve,reject)=>{
+//     let buffers = [];
+//     ctx.req.on('data',function(data){
+//         buffers.push(data);
+//     });
+//     ctx.req.on('end',function(){
+//         ctx.request.body = Buffer.concat(buffers)
+//         resolve();
+//     });
+// });
+
+module.exports = protobufParser
