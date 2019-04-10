0d5fd7be8d4aafd68c01e7d24f590bac49f30b59
jianxun <jianxun@guanghe.tv>
2019-04-10 16:15:43 +0800

    [feat] complete koa protobuf parser middleware

--- koa-protobuf/middleware/protobufParser.js
+++ koa-protobuf/middleware/protobufParser.js
@@ -1,16 +1,29 @@
+const fs = require('fs')
 const getRawBody = require('raw-body')
+const protobuf = require('protobufjs')
 
-const protobufParser = async (ctx, next) => {
-    const p = /^application\/protobuf;/
-    const contentType = ctx.get('content-type')
-    if (!contentType) {
-        return await next()
+
+const protobufParser = (protoPath, namespace) => {
+    if (!fs.existsSync(protoPath)) {
+        throw new Error(`protoPath: ${protoPath}, 对应的文件不存在`)
     }
-    if (p.test(contentType.toLowerCase())) {
-        await getRawBody(ctx.req)
-        return await next()
+    const root = protobuf.loadSync(protoPath)
+    const Message = root.lookupType(namespace)
+
+    return async (ctx, next) => {
+        const p = /^application\/protobuf;/
+        const contentType = ctx.get('content-type')
+        if (!contentType) {
+            return await next()
+        }
+        if (p.test(contentType.toLowerCase())) {
+            const buf = await getRawBody(ctx.req)
+            const decodeObj = Message.decode(buf).toJSON()
+            ctx.request.body = decodeObj
+            return await next()
+        }
+        await next()
     }
-    await next()
 }
 
 // await new Promise((resolve,reject)=>{
