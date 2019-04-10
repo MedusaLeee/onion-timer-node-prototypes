026ba8c6d18b9b371630d2a0db506ce4df5bf006
medusa,leee <medusa.leee@gmail.com>
2019-04-10 23:09:41 +0800

    [feat] add basic tcp server and client

---
+++ tcp-protobuf/basicClient.js
@@ -0,0 +1,12 @@
+const net = require('net')
+
+const client = net.connect({port:8000},function(){
+    console.log('server disconnect')
+})
+client.on('data',function(data){
+    console.log(data.toString())
+    // client.end()
+})
+client.on('end',function(){
+    console.log('client disconnect')
+})
---
+++ tcp-protobuf/basicServer.js
@@ -0,0 +1,25 @@
+/**
+ * package 结构
+ * |------------------|---------------|
+ * |      4 bytes     |    N bytes    |
+ * |------------------|---------------|
+ * |  header(length)  |     proto     |
+ * |------------------|---------------|
+ *
+ */
+
+const  net = require('net')
+
+const server = net.createServer(socket => {
+    socket.on('data', data => {
+        socket.write("hello")
+    })
+    socket.on('end', data => {
+        console.log('disconnect')
+    })
+    socket.write("hello world.\n")
+})
+
+server.listen(8000,function(){
+    console.log('listen 8000')
+})
